3da53e9771050045fb6bf979ffaa25faefe73ac1
anatawa12 <anatawa12@icloud.com>
2021-08-05 23:04:45 +0900

    remove url information

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,15 +72,3 @@ publishing.publications.create<MavenPublication>("maven") {
         }
     }
 }
-
-fun isReleaseBuild() = !version.toString().contains("SNAPSHOT")
-
-fun getReleaseRepositoryUrl(): String {
-    return project.findProperty("RELEASE_REPOSITORY_URL")?.toString()
-        ?: "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-}
-
-fun getSnapshotRepositoryUrl(): String {
-    return project.findProperty("SNAPSHOT_REPOSITORY_URL")?.toString()
-        ?: "https://oss.sonatype.org/content/repositories/snapshots/"
-}
